9e0fbdf09b6995005b3311e7eafc909e759f9ef1
samwelch1 <samuel.h.welch@vanderbilt.edu>
2020-01-09 14:47:22 -0600

    Completed welcome page and player card component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
+import WelcomePage from './WelcomePage'
+import PlayerCard from "./PlayerCard";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <p>Chuka has made changes</p>
+          <WelcomePage/>
+          <PlayerCard/>
       </header>
     </div>
   );
