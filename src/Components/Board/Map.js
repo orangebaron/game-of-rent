89726d315dec1677aae4b963cae49663732ab660
jdnhix <jdhix98890@yahoo.com>
2020-01-16 09:42:27 -0600

    fix

---
+++ src/Components/Board/Map.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+import "./Map.css"
+import Nash from './img/Nashville-map.jpeg';
+
+function Map() {
+    return (
+        <div className="Map">
+            <img src={Nash} className="Nashville-map" />
+        </div>
+    );
+}
+
+export default Map;
+
+
