48d3a8381c1d8a9e202d2f3773dbc5939d91dbe7
Chinchuluun1029 <chinchuluun1029@gmail.com>
2020-01-29 16:25:08 -0600

    handled merge conflict messages

--- src/App.js
+++ src/App.js
@@ -1,37 +1,13 @@
 import React from 'react';
-<<<<<<< HEAD
 import WelcomePage from './Components/WelcomePage/WelcomePage'
-import PlayerCard from "./Components/PlayerCard/PlayerCard";
 
 import './App.css';
-import TextInputComponent from './TextInputComponent.js';
-import ButtonComponent from './ButtonComponent.js'
-
-
-import Card from './Components/Card/CustomCard'
-=======
-import WelcomePage from './Components/WelcomePage/WelcomePage';
-import PlayerCard from './Components/PlayerCard/PlayerCard';
-
-import './App.css';
-import TextInputComponent from './TextInputComponent.js';
-import ButtonComponent from './ButtonComponent.js';
-
-
-import Card from './Components/Card/CustomCard';
->>>>>>> reconnecting to repo
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-<<<<<<< HEAD
-        <WelcomePage/>
-        <PlayerCard/>
-        <Card setOccupation={'retail'} />
-=======
         <WelcomePage />
->>>>>>> reconnecting to repo
       </header>
     </div>
   );
