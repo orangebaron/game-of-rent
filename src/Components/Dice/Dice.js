630dca9d12bf03626a0409305ed9297a7421f483
Chinchuluun1029 <chinchuluun1029@gmail.com>
2020-02-09 17:06:43 -0600

    added Dice view (incomplete), manually linted

--- src/Components/Dice/Dice.js
+++ src/Components/Dice/Dice.js
@@ -1,9 +1,40 @@
 import React from 'react';
 
-export default function Dice() {
+import ReactDice from 'react-dice-complete';
+import 'react-dice-complete/dist/react-dice-complete.css';
+
+export default class Dice extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      numberRolled: 0,
+    };
+  }
+
+  //   rollDoneCallback(num) {
+  //     // this.setState(() => ({
+  //     //   numberRolled: num,
+  //     // }));
+  //     // console.log(`You rolled a ${num}`);
+  //   }
+
+  render() {
+    const { numberRolled } = this.state;
     return (
-        <div>
-            <h1>Hello from Dice</h1>
-        </div>
+      <div style={{ border: '1px black solid' }}>
+        <h1>Hello from Dice</h1>
+        <h1>
+          You rolled:
+          {' '}
+          {numberRolled}
+        </h1>
+        <ReactDice
+          numDice={1}
+          faceColor="#ffffff"
+          dotColor="#000000"
+          rollDone={this.rollDoneCallback}
+        />
+      </div>
     );
+  }
 }
