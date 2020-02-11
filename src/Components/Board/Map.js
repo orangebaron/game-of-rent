967d35e403bf194d7984a0765dec6237198ebca5
jdnhix <jdhix98890@yahoo.com>
2020-02-11 10:27:09 -0600

    temp fix for map and grid

--- src/Components/Board/Map.js
+++ src/Components/Board/Map.js
@@ -1,16 +1,17 @@
 import React, { useState } from 'react';
-import "./Map.css"
-import Nash from './img/Nashville-map.jpg'
+import './Map.css';
+import Nash from './img/Nashville-map-cropped.jpg';
+import Letters from './img/Grid Letters.png';
+import Numbers from './img/Grid Numbers.png';
 
 function Map() {
-    return (
-        <div className="Map">
-            <img src={Nash} className="Nashville-map" />
-            <p className="Map-extraLetter">
-                S
-            </p>
-        </div>
-    );
+  return (
+    <div className="Map">
+      <img src={Numbers} alt="numbers for map grid" className="gridNumbers" />
+      <img src={Letters} alt="letters for map grid" className="gridLetters" />
+      <img src={Nash} className="Nashville-map" />
+    </div>
+  );
 }
 
 export default Map;
