66568a58df31d10320e94b4c42385696462d63c3
Chinchuluun1029 <chinchuluun1029@gmail.com>
2020-02-15 14:41:13 -0600

    removed unnecessary from PlayerCard

--- src/Components/PlayerCard/PlayerCard.js
+++ src/Components/PlayerCard/PlayerCard.js
@@ -1,9 +1,5 @@
 import React from 'react';
-import infoBubble from './img/infoBubble.png';
-import './PlayerCard.css';
 import PlayerIcon from '../PlayerIcon/PlayerIcon';
-
-
 import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
@@ -47,25 +43,6 @@ function PlayerCard() {
         </Button>
       </Paper>
     </Grid>
-
-//     <div className="PlayerCard">
-//       <header className="PlayerCard-header">
-
-//         <p className="PlayerCard-Name">
-//                     John Doe
-//         </p>
-//         <PlayerIcon num="0" className="PlayerCard-Avatar" alt="player avatar" />
-//         <p className="PlayerCard-Summary">
-//                     Family Monthly Income:
-//           {' '}
-//           <br />
-// $2,500
-//         </p>
-//         <div className="PlayerCard-infoIcon">
-//           <img src={infoBubble} alt="logo" />
-//         </div>
-//       </header>
-//     </div>
   );
 }
 
