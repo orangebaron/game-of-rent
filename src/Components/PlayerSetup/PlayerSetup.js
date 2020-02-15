9bd18d43aa0568a21a1d3a8235b430af2e531bdd
jdnhix <jdhix98890@yahoo.com>
2020-02-15 15:00:51 -0600

    add player setup view

--- src/Components/PlayerSetup/PlayerSetup.js
+++ src/Components/PlayerSetup/PlayerSetup.js
@@ -1,6 +1,61 @@
 import React from 'react';
+import Background from '../WelcomePage/img/croppedBoard.png';
+import PlayerIcon from '../PlayerIcon/PlayerIcon';
 
-const setup = ['Setup page 1', 'Setup page 2', '...'];
+const setup = ['Player 1', 'Player 2', '...'];
+
+const styles = {
+  mapBackground: {
+    width: '100vw',
+    height: 'auto',
+    backgroundImage: `url(${Background})`,
+    backgroundPosition: 'center',
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: 'cover',
+  },
+  playerSetup: {
+    height: '100vh',
+    display: 'flex',
+    justifyContent: 'space-evenly',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  textGrid: {
+    backgroundColor: '#000000aa',
+    borderRadius: '1em',
+    width: '75vw',
+    height: '70vh',
+    padding: '2%',
+    display: 'flex',
+    justifyContent: 'space-evenly',
+    flexDirection: 'column',
+    alignItems: 'center',
+    color: 'white',
+  },
+  fields: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  textBox: {
+    border: 0,
+    width: '30vw',
+    height: '5vh',
+    borderRadius: 10,
+
+  },
+  avatarSection: {
+
+    width: '50vw',
+    display: 'flex',
+    justifyContent: 'space-evenly',
+    marginTop: '2vh',
+  },
+  playerAvatar: {
+    cursor: 'pointer',
+  },
+};
+
 
 class PlayerSetup extends React.Component {
   constructor(props) {
@@ -18,11 +73,35 @@ class PlayerSetup extends React.Component {
 
   render() {
     return (
-      <div
-        className="PlayerSetup"
-        style={{ backgroundColor: 'gray' }}
-      >
-        <p>{setup[this.state.setupLocation]}</p>
+      <div style={styles.mapBackground}>
+        <div style={styles.playerSetup}>
+          <h1>{setup[this.state.setupLocation]}</h1>
+          <div style={styles.textGrid}>
+
+            <div style={styles.fields}>
+              <h2>Enter a name for your player</h2>
+              <input type="text" style={styles.textBox} />
+            </div>
+
+            <div style={styles.fields}>
+              <h2>Choose an avatar for your player</h2>
+              <div style={styles.avatarSection}>
+                <PlayerIcon num="0" style={styles.playerAvatar} />
+                <PlayerIcon num="1" style={styles.playerAvatar} />
+                <PlayerIcon num="2" style={styles.playerAvatar} />
+                <PlayerIcon num="3" style={styles.playerAvatar} />
+                <PlayerIcon num="4" style={styles.playerAvatar} />
+                <PlayerIcon num="5" style={styles.playerAvatar} />
+              </div>
+
+
+            </div>
+
+
+          </div>
+
+
+        </div>
       </div>
     );
   }
