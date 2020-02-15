3737f8da3f594bf69bb1ea0c74ce325ff18bc8e2
Chinchuluun1029 <chinchuluun1029@gmail.com>
2020-02-15 12:14:52 -0600

    fixed WelcomePage styling

--- src/Components/WelcomePage/WelcomePage.js
+++ src/Components/WelcomePage/WelcomePage.js
@@ -27,25 +27,25 @@ const headerStyle = {
   width: 'auto',
   color: '#ffffff',
   display: 'flex',
-  padding: '0 20%',
   flexDirection: 'column',
   alignItems: 'baseline',
   justifyContent: 'center',
-}
+};
 
 const textGrid = {
   backgroundColor: '#000000aa',
   borderRadius: '1em',
+  width: '65vw',
   padding: '2%',
   textAlign: 'center',
   fontSize: '1.5em',
-}
+};
 
-const buttonStyle= {
+const buttonStyle = {
   color: '#ffffff',
   fontWeight: 700,
   border: '2px solid #ffffff',
-}
+};
 
 
 // .WelcomePage-introText{
@@ -70,30 +70,28 @@ const buttonStyle= {
 //   cursor: pointer;
 // }
 
-
-
 function WelcomePage() {
   return (
     <div style={sectionStyle}>
-        <header style={headerStyle}>
-          <div style={textGrid}>
-            <p>
-              Welcome to the Game of Rent!
-            </p>
-            <p className="WelcomePage-introText">
-              More and more American families and individuals are finding
-              it increasingly difficult to live
-              somewhere they can afford. In the Game of Rent, each player
-              will have to make the most of what
-              they’ve given and find an apartment you can afford to rent
-              as close to your job as possible.
-            </p>
-            <Button variant="outlined" style={buttonStyle} href="/board">
-              Start
-            </Button>
-          </div>
-        </header>
-      </div>
+      <header style={headerStyle}>
+        <div style={textGrid}>
+          <p>
+            Welcome to the Game of Rent!
+          </p>
+          <p className="WelcomePage-introText">
+            More and more American families and individuals are finding
+            it increasingly difficult to live
+            somewhere they can afford. In the Game of Rent, each player
+            will have to make the most of what
+            they’ve given and find an apartment you can afford to rent
+            as close to your job as possible.
+          </p>
+          <Button variant="outlined" style={buttonStyle} href="/board">
+            Start
+          </Button>
+        </div>
+      </header>
+    </div>
   );
 }
 
