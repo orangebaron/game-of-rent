868d093beef9e0494fc384b8d9ba2f7d598d3277
Sam Alws <sam.alws@gmail.com>
2020-05-01 16:27:31 -0400

    space between icons

--- src/Components/PlayerRadio/PlayerRadio.js
+++ src/Components/PlayerRadio/PlayerRadio.js
@@ -3,7 +3,7 @@ import PlayerIcon from '../PlayerIcon/PlayerIcon';
 
 function PlayerRadio(props) {
   return (
-    <label style={{cursor: "pointer"}}>
+    <label style={{cursor: "pointer", margin: "4px"}}>
         <input type="radio" name={props.name} value={props.num} style={{position: "absolute", opacity: 0, width: 0}} />
         <PlayerIcon num={props.num} />
     </label>
