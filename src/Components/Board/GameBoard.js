1be9768b2340f935b203f30abad2eced146e7649
Sam Alws <sam.alws@gmail.com>
2020-05-15 14:46:58 -0400

    card flipping stuff

--- src/Components/Board/GameBoard.js
+++ src/Components/Board/GameBoard.js
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import './GameBoard.css';
 import { makeStyles } from '@material-ui/core/styles';
 import Map from './Map';
 import PlayerCard from '../PlayerCard/PlayerCard';
+import FlippingCard from '../FlippingCard/FlippingCard';
 import OccupationCardBack from '../Card/img/GameOfRent_OccupationBack.jpg';
 import LifeCardBack from '../Card/img/GameOfRent_LifeBack.jpg';
 import NeighborhoodCardBack from '../Card/img/GameOfRent_NeighborhoodBack.jpg';
@@ -41,38 +42,44 @@ const useStyles = makeStyles(() => ({
   },
 }));
 
-function showCardFullscreen() {
+function showCardFullscreen(ref, cardId) {
   document.getElementById("overlay").style.display = "block"; // TODO getting by id is probably bad
+  ref.startForRightCard(document.getElementById(cardId));
 }
 
-function closeFullscreenCard() {
-  document.getElementById("overlay").style.display = "none"; // TODO getting by id is probably bad
+function showPlayerCardFullscreen(ref) {
+  document.getElementById("overlay").style.display = "block"; // TODO getting by id is probably bad
+  ref.startForLeftCard();
+}
+
+function closeFullscreenCard(ref) {
+  ref.goFromCenter(() => document.getElementById("overlay").style.display = "none");
 }
 
 function GameBoard() {
   const classes = useStyles();
-
+  const sketchyRef = {}; // passed as an argument to FlippingCard; FlippingCard sets sketchyRef.ref = this; then used in button click handlers
+  // there must be a better way to do this
   return (
     <div className={classes.root}>
       <div id="overlay" style={{height:"100%", width:"100%", backgroundColor:"rgba(0,0,0, 0.5)", zIndex:1, position:"fixed", display:"none"}}>
-        <p onClick={closeFullscreenCard} style={{position:"fixed",left:"90%",color:"white",fontWeight:"bold",cursor:"pointer"}}>X</p>
-        <img style={{ height: '160px',left:"40%",top:"30%",position:"fixed" }} src={OccupationCardBack} className="card" alt="OccupationCardBack" />
-        // TODO make this img be good
+        <p onClick={() => closeFullscreenCard(sketchyRef.ref)} style={{position:"fixed",left:"90%",color:"white",fontWeight:"bold",cursor:"pointer"}}>X</p>
+        <FlippingCard sketchyRef={sketchyRef} startSize={[0, 0]} startXY={[0, 0]} />
       </div>
       <div className={classes.playerCardSection}>
-        <PlayerCard />
-        <PlayerCard />
-        <PlayerCard />
-        <PlayerCard />
+        <PlayerCard onClick={() => showPlayerCardFullscreen(sketchyRef.ref)} />
+        <PlayerCard onClick={() => showPlayerCardFullscreen(sketchyRef.ref)} />
+        <PlayerCard onClick={() => showPlayerCardFullscreen(sketchyRef.ref)} />
+        <PlayerCard onClick={() => showPlayerCardFullscreen(sketchyRef.ref)} />
       </div>
       <div className={classes.map}>
         <Map />
       </div>
       <div className={classes.gameCardSection}>
-        <img style={{ height: '160px' }} src={OccupationCardBack} className="card" alt="OccupationCardBack" onClick={showCardFullscreen} />
-        <img style={{ height: '160px' }} src={HouseholdCardBack} className="card" alt="HouseholdCardBack" onClick={showCardFullscreen} />
-        <img style={{ height: '160px' }} src={LifeCardBack} className="card" alt="LifeCardBack" onClick={showCardFullscreen} />
-        <img style={{ height: '160px' }} src={NeighborhoodCardBack} className="card" alt="NeighborhoodCardBack" onClick={showCardFullscreen} />
+        <img style={{ height: '160px'}} id="occupationCardBack"   src={OccupationCardBack}   className="card" alt="OccupationCardBack"   onClick={() => showCardFullscreen(sketchyRef.ref, "occupationCardBack")} />
+        <img style={{ height: '160px'}} id="householdCardBack"    src={HouseholdCardBack}    className="card" alt="HouseholdCardBack"    onClick={() => showCardFullscreen(sketchyRef.ref, "householdCardBack")} />
+        <img style={{ height: '160px'}} id="lifeCardBack"         src={LifeCardBack}         className="card" alt="LifeCardBack"         onClick={() => showCardFullscreen(sketchyRef.ref, "lifeCardBack")} />
+        <img style={{ height: '160px'}} id="neighborhoodCardBack" src={NeighborhoodCardBack} className="card" alt="NeighborhoodCardBack" onClick={() => showCardFullscreen(sketchyRef.ref, "neighborhoodCardBack")} />
       </div>
 
     </div>
