3cc13b4eb28f4c9d6b3a0aad1632f04681c16517
Sam Alws <sam.alws@gmail.com>
2020-05-29 02:58:36 -0400

    dice on board

--- src/Components/Board/Map.js
+++ src/Components/Board/Map.js
@@ -4,6 +4,7 @@ import { makeStyles } from '@material-ui/core/styles';
 import Nash from './img/Nashville-map-cropped.jpg';
 import Letters from './img/Grid Letters.png';
 import Numbers from './img/Grid Numbers.png';
+import Dice from '../Dice/Dice.js';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -29,12 +30,18 @@ const useStyles = makeStyles((theme) => ({
 function Map() {
   const classes = useStyles();
 
+  // the Dice component is just a wrapper for ReactDice, so why not just use that?
+
   return (
     <div className={classes.root}>
       {/* <img width= src={Numbers} alt="numbers for map grid" className="gridNumbers" /> */}
       {/* <img src={Letters} alt="letters for map grid" className="gridLetters" /> */}
       <img src={Nash} className={classes.map} alt="Nashville Map" />
-      {/* need to add dice to map */}
+      <div style={{position:"relative", left:-90, top:"50%"}}>
+        <div style={{position:"relative",top:-90}}>
+          <Dice />
+        </div>
+      </div>
     </div>
   );
 }
